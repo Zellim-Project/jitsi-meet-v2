1f22493452cba2bebd13935d4f1d648111c9f626
Christian Curi <c.christiancuri@gmail.com>
2021-08-06 13:50:48 -0300

    feat: branding

--- react/custom/components/CustomBackground/index.js
+++ react/custom/components/CustomBackground/index.js
@@ -49,28 +49,88 @@ const Background = styled.div`
         bottom: 0;
         transform: scale(-1);
         -webkit-transform: scale(-1);
-
     }
 `;
 
-const CustomBackground = () => (
-    <Container>
-        <Background className = 'topLeft'>
-            <img src = 'images/background.png' />
-        </Background>
+const BrandingBackgroundContainer = styled.div`
+    position: fixed;
 
-        <Background className = 'topRight'>
-            <img src = 'images/background.png' />
-        </Background>
+    img {
+        width: 100vw;
+        height: 100vh;
+    }
+`;
 
-        <Background className = 'bottomLeft'>
-            <img src = 'images/background.png' />
-        </Background>
+const BrandingLogoComponent = styled.div`
+    background-image: url(${props => props.image});
+    max-width: 140px;
+    max-height: 70px;
+    position: absolute;
+`
 
-        <Background className = 'bottomRight'>
-            <img src = 'images/background.png' />
-        </Background>
-    </Container>
-);
+function BrandingLogo({ image }) {
+    return <BrandingLogoComponent image={image} className="watermark leftwatermark" />
+}
+
+function BrandingBackground({ image }) {
+    return (
+        <BrandingBackgroundContainer>
+            <img src={image} />
+        </BrandingBackgroundContainer>
+    )
+}
+
+function getRoomName() {
+    const [room] = (window.location.ancestorOrigins.length ? window.location.ancestorOrigins[0] : window.location.href).replace('https://', '').split('.');
+    return room;
+}
+
+const CustomBackground = () => {
+    const [logo, setLogo] = React.useState('');
+    const [background, setBackground] = React.useState('');
+
+    const fetchBranding = async () => {
+        const roomName = getRoomName();
+        const data = await fetch(`https://app.zellim.com/module/chat/conference/rooms/branding/${roomName}`).then(data => data.json());
+
+        if (data?.branding?.logo) {
+            setLogo(data.branding.logo)
+        }
+        if (data?.branding?.background) {
+            setBackground(data.branding.background)
+        }
+    }
+
+    React.useEffect(() => {
+        fetchBranding();
+    }, []);
+
+    return (
+        <Container>
+            {logo && <BrandingLogo image={logo} />}
+            {background ? (
+                <BrandingBackground image={background} />
+            ) : (
+                <>
+                    <Background className = 'topLeft'>
+                        <img src = 'images/background.png' />
+                    </Background>
+
+                    <Background className = 'topRight'>
+                        <img src = 'images/background.png' />
+                    </Background>
+
+                    <Background className = 'bottomLeft'>
+                        <img src = 'images/background.png' />
+                    </Background>
+
+                    <Background className = 'bottomRight'>
+                        <img src = 'images/background.png' />
+                    </Background>
+                </>
+            )}
+        </Container>
+    )
+};
 
 export default CustomBackground;
